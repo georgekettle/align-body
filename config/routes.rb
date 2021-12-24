54d5af16cf026475e2493030d5d9ffa00239ae77
george kettle <georgekettle.gk@gmail.com>
2021-12-24 15:34:27 +1100

    workouts controler created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :instructors, only: [:show, :index]
+  resources :workouts, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
