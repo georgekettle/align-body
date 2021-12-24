2b2562e0445834345151769cf5ad59d34f01d477
george kettle <georgekettle.gk@gmail.com>
2021-12-24 16:32:33 +1100

    saves index

--- app/controllers/saves_controller.rb
+++ app/controllers/saves_controller.rb
@@ -1,2 +1,5 @@
 class SavesController < ApplicationController
+	def index
+		@workouts = current_user.workouts
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :instructors, only: [:show, :index]
   resources :workouts, only: [:show, :index]
+  resources :saves, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
