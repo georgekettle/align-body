ab5f05f222b3b8084ba50354700708393a936d45
george kettle <georgekettle.gk@gmail.com>
2021-12-24 15:51:02 +1100

    Seed file updated

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,5 @@
 class WorkoutsController < ApplicationController
+	def index
+		@workouts = Workout.includes(:category, :instructor).all
+	end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,7 +2,7 @@ class Workout < ApplicationRecord
   belongs_to :category
   belongs_to :instructor
 
-  enum status: {
+  enum difficulty: {
     easy: 0,
     medium: 1,
     hard: 2
