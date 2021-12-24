d9af39409b717ba2a1f0dc2aae2f5e455dabe892
george kettle <georgekettle.gk@gmail.com>
2021-12-24 17:43:06 +1100

    Save functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :instructors, only: [:show, :index]
-  resources :workouts, only: [:show, :index]
+  resources :workouts, only: [:show, :index] do
+    post 'toggle_save', to: 'saves#toggle', as: :toggle_save
+  end
   resources :saves, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
