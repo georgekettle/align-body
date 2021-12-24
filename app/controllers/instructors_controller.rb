cb43cb2198e19319f262e4241c49e09529bd8c9a
george kettle <georgekettle.gk@gmail.com>
2021-12-24 15:15:38 +1100

    Instructors index

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,7 +1,11 @@
 class InstructorsController < ApplicationController
-	skip_before_action :authenticate_user!, only: [:show]
+	skip_before_action :authenticate_user!, only: [:show, :index]
 
 	def show
 		@instructor = Instructor.find(params[:id])
+	end
+
+	def index
+		@instructors = Instructor.all
 	end
 end
