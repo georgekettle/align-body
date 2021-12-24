94351fcca3d91acade669ee16e2bf3086a9ea968
george kettle <georgekettle.gk@gmail.com>
2021-12-24 15:58:06 +1100

    workouts show and updated schema

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,4 +2,8 @@ class WorkoutsController < ApplicationController
 	def index
 		@workouts = Workout.includes(:category, :instructor).all
 	end
+
+	def show
+		@workout = Workout.find(params[:id])
+	end
 end
