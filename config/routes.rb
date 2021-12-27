e0cfbfff018637455a0e5e429c8f787f77fab624
george kettle <georgekettle.gk@gmail.com>
2021-12-27 18:30:04 +1100

    Registrations controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   resources :instructors, only: [:show, :index]
   resources :workouts, only: [:show, :index] do
