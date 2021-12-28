c3d9f1f12d8e6b1e40299b5679c9f7d5fc22b72c
george kettle <georgekettle.gk@gmail.com>
2021-12-28 16:53:51 +1100

    skip instructors pundit policy

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,5 +1,7 @@
 class InstructorsController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:show, :index]
+	skip_after_action :verify_authorized, only: :show
+	skip_after_action :verify_policy_scoped, only: :index
 
 	def show
 		@instructor = Instructor.find(params[:id])
