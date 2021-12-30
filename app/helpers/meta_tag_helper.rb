5eaa1246a5d2bc29595b3cf4af37cafb397a4bdd
george kettle <georgekettle.gk@gmail.com>
2021-12-30 11:16:54 +1100

    Meta Tags added to each page

--- app/helpers/meta_tag_helper.rb
+++
@@ -1,9 +0,0 @@
-module MetaTagHelper
-	def meta_tag(tag, text)
-    content_for :"meta_#{tag}", text
-  end
-
-  def yield_meta_tag(tag, default_text='')
-    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
-  end
-end
