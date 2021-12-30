ac4967bd69f705813ea7753b7e98b4fa43eb4fab
george kettle <georgekettle.gk@gmail.com>
2021-12-30 18:11:44 +1100

    recombee concern added to workout model

--- app/services/recommender_service.rb
+++ app/services/recommender_service.rb
@@ -3,39 +3,69 @@ require 'recombee_api_client'
 class RecommenderService
 	include RecombeeApiClient
 
+	COLUMN_TYPE = {
+		string: 'string',
+		enum: 'string',
+		integer: 'int',
+		float: 'double',
+		boolean: 'boolean',
+		datetime: 'timestamp'
+	}
+
   cattr_reader :recombee_client, instance_accessor: false do
-    RecombeeClient(ENV['RECOMBEE_DB_ID'], ENV['RECOMBEE_PRIVATE_TOKEN'])
+    RecombeeApiClient::RecombeeClient.new(ENV['RECOMBEE_DB_ID'], ENV['RECOMBEE_PRIVATE_TOKEN'])
   end
 
+  def self.send_batch(items)
+  	recombee_client.send(Batch.new(items))
+  rescue RecombeeApiClient::ResponseError => error
+  	p error
+  end
+
+  # ITEMS
   def self.add_item(item)
-  	recombee_client.send(AddItem.new(item.recombee_id))
+  	AddItem.new(item.recombee_id)
   end
 
-  def self.add_item(property_name, type)
-  	recombee_client.send(AddItemProperty.new(property_name, type))
+  def self.delete_item(item)
+  	DeleteItem.new(item.recombee_id)
   end
 
+  def self.add_item_property(property_name, type)
+  	AddItemProperty.new(property_name, type)
+  end
+
+  def self.set_item_values(item, values)
+  	SetItemValues.new(item.recombee_id, values, { cascade_create: true })
+  end
+
+  # USERS
+  def self.add_user(user)
+  	recombee_client.send(AddUser.new(user.id))
+  end
+
+  # INTERACTIONS
   def self.report_view(user, item)
-  	recombee_client.send(AddDetailView.new(user.recombee_id, item.recombee_id, {cascade_create: true}))
+  	recombee_client.send(AddDetailView.new(user.recombee_id, item.recombee_id, { cascade_create: true }))
   end
 
   def self.report_purchase(user, item)
-  	recombee_client.send(AddPurchase.new(user.recombee_id, item.recombee_id, {cascade_create: true}))
+  	recombee_client.send(AddPurchase.new(user.recombee_id, item.recombee_id, { cascade_create: true }))
   end
 
   def self.report_bookmark(user, item, recomm_id = nil)
   	if recomm_id
-  		recombee_client.send(AddBookmark.new(user.recombee_id, item.recombee_id, {cascade_create: true, recomm_id: recomm_id}))
+  		recombee_client.send(AddBookmark.new(user.recombee_id, item.recombee_id, { cascade_create: true, recomm_id: recomm_id }))
   	else
-  		recombee_client.send(AddBookmark.new(user.recombee_id, item.recombee_id, {cascade_create: true}))
+  		recombee_client.send(AddBookmark.new(user.recombee_id, item.recombee_id, { cascade_create: true }))
   	end
   end
 
   def self.report_view_portion(user, item, portion, session_id, recomm_id = nil)
   	if recomm_id
-  		recombee_client.send(SetViewPortion.new(user.recombee_id, item.recombee_id, portion, {cascade_create: true, session_id: session_id, recomm_id: recomm_id}))
+  		recombee_client.send(SetViewPortion.new(user.recombee_id, item.recombee_id, portion, { cascade_create: true, session_id: session_id, recomm_id: recomm_id }))
   	else
-  		recombee_client.send(AddBookmark.new(user.recombee_id, item.recombee_id, portion, {cascade_create: true, session_id: session_id}))
+  		recombee_client.send(AddBookmark.new(user.recombee_id, item.recombee_id, portion, { cascade_create: true, session_id: session_id }))
   	end
   end
 end
