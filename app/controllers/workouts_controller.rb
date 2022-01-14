832b18e43aad83199ce6c744da17f9a6d1e1e021
george kettle <georgekettle.gk@gmail.com>
2022-01-15 10:41:58 +1100

    Updated low/high intensity statuis

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,7 +2,7 @@ class WorkoutsController < ApplicationController
 	def index
 		recommendations = RecommenderService.send_batch([ RecommenderService.get_recommendations(current_user, 20, 'workouts_index') ])
 		recommended_workout_ids = recommendations ? recommendations.first['json']['recomms'].map{ |item| item["id"].gsub(/[^\d]/, '') } : []
-		@recomm_id = recommendations.first['json']['recommId']
+		@recomm_id = recommendations&.first['json']['recommId'] || nil
 		@workouts = Workout.includes(:category, :instructor).where(id: recommended_workout_ids)
 		@categories = Category.all
 		policy_scope(@workouts)
