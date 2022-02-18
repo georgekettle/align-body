3b8c208cc7de7d893eed7103ddd24008ae8ba159
george kettle <georgekettle.gk@gmail.com>
2022-02-18 14:36:09 +1100

    Added sendgrid as email provider

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # further smtp settings in config/initializers/smtp.rb
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    domain: ENV['DOMAIN'],
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: 'plain',
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY']
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
