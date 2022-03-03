0d2cff3764662290f4d0944f4c2965124b5a52c6
george kettle <georgekettle.gk@gmail.com>
2022-03-03 18:14:51 +1100

    Simplified version of page transition

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "helpers/cookies"
-import "helpers/native"
+import * as Native from "helpers/native"
 import "controllers"
 
 document.addEventListener('turbo:load', (e) => {
-	hideSplashScreen() // for native
+	Native.hideSplashScreen()
 })
+
+document.addEventListener('turbo:before-fetch-request', Native.transitionOut)
+document.addEventListener('turbo:before-render', Native.transitionIn)
